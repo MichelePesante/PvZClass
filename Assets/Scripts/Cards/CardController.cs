b5df0aa4237ad9eb461666fb3e151c33798719d1
EasterMaster <teo.mastropasqua@gmail.com>
2018-12-18 12:39:39 +0100

    Created CardController

---
+++ Assets/Scripts/Cards/CardController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour {
+
+    public CardData cardData;
+
+    [HideInInspector]
+    public Sprite CardImage;
+    [HideInInspector]
+    public int Cost, Attack, Life;
+    [HideInInspector]
+    public bool IsHeroCard;
+    [HideInInspector]
+    public CardData.Rarity Rarity;
+
+    public void SetUP()
+    {
+        CardImage = cardData.CardImage;
+        Rarity = cardData.CardRarity;
+        Cost = cardData.Cost;
+        Attack = cardData.Attack;
+        Life = cardData.Life;
+        IsHeroCard = cardData.IsHeroCard;
+    }
+}
