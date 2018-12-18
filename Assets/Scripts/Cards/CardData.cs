535270decc023d71a870dad6c497f30056009337
MarcoMarchitelli <igvvcdhk@gmail.com>
2018-12-18 12:28:24 +0100

    Creata scene cardtest, creato scriptable e example obj.

---
+++ Assets/Scripts/Cards/CardData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "New Card")]
+public class CardData : ScriptableObject {
+
+    public string CardName;
+    public int Cost;
+    public int Life;
+    public int Attack;
+    public Rarity CardRarity;
+    public bool IsHeroCard;
+    public Sprite CardImage; 
+
+    public enum Rarity { common, uncommon, rare, legendary}
+}
