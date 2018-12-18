8dd550d5117e4d8fb6e2c3d5e5b8300eb3669aef
emanuelepapagno <emanuelelele92@gmail.com>
2018-12-18 12:56:00 +0100

    creata classe Player e interfaccia IPlayer

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    int life;
+    int energy;
+    int shield;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
