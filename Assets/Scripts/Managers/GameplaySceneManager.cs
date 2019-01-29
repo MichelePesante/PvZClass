5769ddbc1aa3d4b1af394a0cd61e6b9cb8335ce3
brago <bragolab@gmail.com>
2019-01-29 09:45:09 +0100

    Base per mulligan

---
+++ Assets/Scripts/Managers/GameplaySceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplaySceneManager : MonoBehaviour {
+
+    public Player player, player2;
+    [Header("UI Elements")]
+    public Canvas GlobalUI;
+    public MulliganController MulliganP1;
+    public MulliganController MulliganP2;
+    public BoardController BoardController;
+
+
+    public void Setup() {
+
+        
+
+        MulliganP1.Init()
+            MulliganP2.Init()
+    }
+
+}
