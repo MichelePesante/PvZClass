45dfc98bb370da0fefbfef0edd0fee1f614a27cd
EasterMaster <teo.mastropasqua@gmail.com>
2019-02-12 13:30:16 +0100

    Add Win text

--- Assets/Scripts/StateMachine/CustomGameplay/StateBehaviours/GP_MatchResult_State.cs
+++ Assets/Scripts/StateMachine/CustomGameplay/StateBehaviours/GP_MatchResult_State.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 namespace StateMachine.Gameplay {
     public class GP_MatchResult_State : GP_BaseState {
 
+        [SerializeField]
+        GameObject WinTextPrefab;
+        GameObject WinTextGO;
+
         public override void Enter()
         {
             base.Enter();
-            Debug.Log(context.Winner);
+            if (WinTextPrefab != null)
+            {
+                WinTextGO = Instantiate(WinTextPrefab, context.UICanvas.transform);
+                WinTextGO.GetComponent<Text>().text = context.Winner.CurrentType + " win";
+            }
         }
     }
 }
