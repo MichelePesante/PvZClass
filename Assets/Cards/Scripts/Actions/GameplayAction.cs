147d14fc7a7197b653dffc1da27940c6b93a5499
Paolo Bragonzi <brago@indieconstruction.com>
2019-04-16 12:20:10 +0200

    Added GameplayAction

---
+++ Assets/Cards/Scripts/Actions/GameplayAction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameplayAction : IAction {
+
+    public CardData cardData;
+    public DeckData deckDataFrom;
+    public DeckData deckDataTo;
+
+
+    public ActionType Type { get; set; }
+
+    public static GameplayAction CreateMovementAction(CardData _cardData, DeckData _deckDataFrom, DeckData _deckDataTo) {
+        GameplayAction returnAction = new GameplayAction() {
+            cardData = _cardData,
+            deckDataFrom = _deckDataFrom,
+            deckDataTo = _deckDataTo,
+            Type = ActionType.Movement,
+        };
+
+        return returnAction;
+    }
+
+
+}
+
+public enum ActionType {
+        Movement = 10,
+}
+
+public interface IAction {
+    ActionType Type { get; set; }
+}
