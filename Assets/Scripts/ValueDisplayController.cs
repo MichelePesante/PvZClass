5bec1cd4c15f8402c8e417c2582040abd270e8d8
Anna Bertolani <bertolani.anna@gmail.com>
2019-06-06 10:31:22 +0200

    Ciao sono sempre io, Anna! :). Fixed card view back graphics. Fixed card view event subscription issues.

--- Assets/Scripts/ValueDisplayController.cs
+++ Assets/Scripts/ValueDisplayController.cs
@@ -1,41 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ValueDisplayController : MonoBehaviour {
 
     public Text Lable;
-    public Image BGColorImage;
-    public Image ImageEffect;
-    public Color StartColor;
-
-    public bool EnableEffectOnChange;
 
     public Action<string> OnValueChanged;
-
-    private void Awake() {
-        //SetColor(StartColor);
-    }
 
     #region API
 
     public void SetValue(string _value) {
-        Lable.text = _value;
-        EnableEffect(EnableEffectOnChange);
+        if(Lable)
+            Lable.text = _value;
         if (OnValueChanged != null)
             OnValueChanged(_value);
     }
 
-    //public void SetColor(Color _color) {
-    //    if(BGColorImage)
-    //        BGColorImage.color = _color;
-    //}
-
-    public void EnableEffect(bool enableEffect) {
-        ImageEffect.enabled = enableEffect;
-    }
-
     #endregion
-
 }
